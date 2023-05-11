7645cc25c78eccb5a16d5a887eb5db72a01405a6
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-05-11 21:45:05 +0200

    suppression définitive de satellite

--- src/stamps/controleur/PanneauSatellite.java
+++ src/stamps/controleur/PanneauSatellite.java
@@ -5,7 +5,7 @@ import javafx.collections.ObservableArray;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
@@ -14,6 +14,7 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import stamps.model.CollectionSatellites;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -22,8 +23,7 @@ public class PanneauSatellite extends Controleur {
     public Label nom;
     public HBox hbox;
     private final int posSatellite;
-
-    private ScaleTransition scale;
+    public ContextMenu menuContext;
 
     /**
      * constructeur de la classe
@@ -52,10 +52,7 @@ public class PanneauSatellite extends Controleur {
     @FXML
     void changerDetail(MouseEvent event) throws IOException {
         if(event.getButton() == MouseButton.SECONDARY) {
-            if(!collectionSatellites.isEstConsulte()) {
-                collectionSatellites.supprimer(collectionSatellites.getSatellite(posSatellite));
-                collectionSatellites.notifierObservateurs();
-            }
+            menuContext.show(nom, event.getScreenX(), event.getScreenY());
         }else {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("../vue/PanneauDetail.fxml"));
@@ -70,6 +67,48 @@ public class PanneauSatellite extends Controleur {
             stage.setScene(root);
             stage.show();
         }
+    }
+
+    /**
+     * méthode qui permet de supprimer définitivement un satellite
+     */
+    @FXML
+    void supprimer(){
+        String filePath = "src/ressource/sauvegarde/sat"+collectionSatellites.getSatellite(posSatellite).getIdentifiant()+".json";
+        // Création de l'objet File
+        File file = new File(filePath);
+
+        // Affichage de la boîte de dialogue de confirmation
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation de suppression");
+        alert.setHeaderText("Supprimer le fichier ?");
+        alert.setContentText("Êtes-vous sûr de vouloir supprimer le fichier ?");
+
+        // Attente de la réponse de l'utilisateur
+        alert.showAndWait().ifPresent(response -> {
+            if (response == ButtonType.OK) {
+                // Suppression du fichier
+                boolean deleted = file.delete();
+                if (deleted) {
+                    showAlert(Alert.AlertType.INFORMATION, "Suppression réussie", "Le fichier a été supprimé avec succès.");
+                } else {
+                    showAlert(Alert.AlertType.ERROR, "Erreur de suppression", "La suppression du fichier a échoué.");
+                }
+            }
+        });
+        collectionSatellites.supprimer(collectionSatellites.getSatellite(posSatellite));
+        collectionSatellites.notifierObservateurs();
+    }
+
+    /**
+     * méthode qui affiche un message d'alerte
+     */
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
     public String getNom() {
